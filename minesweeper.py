6d5b67912c355ad92fd16b174cf6f0fa8a1688c4
Ali Shalabi <ashalabi13@gmail.com>
2020-02-27 22:12:54 -0800

    Added method to add random number of mines

--- minesweeper.py
+++ minesweeper.py
@@ -18,6 +18,7 @@ Should yield:
 """
 
 from pprint import pprint
+import random
 
 
 class Minesweeper:
@@ -39,22 +40,44 @@ class Minesweeper:
             (1, 1),
             (0, 1)
         ]
+        self.all_flags = []
 
-    # Add a mine in any valid cell
     def add_mine(self, row_index, col_index):
+        """
+        Add a single mine at a specific, valid location
+        """
         if self._is_valid(row_index, col_index):
-            self.gameboard[row_index][col_index] = "X"
+            self.gameboard[row_index][col_index] = "MINE!"
 
-    # Add a random mine
     def add_random_mines(self, num_mines):
-        pass
+        """
+        Add a random number of mines to the gameboard
+        Will use recursion
+        """
+        # Exit: have placed all mines
+        if num_mines == 0:
+            return
+        # Get random x and y
+        random_x = random.randrange(0, self.width)
+        print(random_x)
+        random_y = random.randrange(0, self.height)
+        print(random_x)
+        # If cell is already a mine, call function again without decreasing mines
+        if self.gameboard[random_x][random_y] == "MINE!":
+            return self.add_random_mines(num_mines)
+        else:
+            self.gameboard[random_x][random_y] = "MINE!"
+            return self.add_random_mines(num_mines - 1)
 
     def generate_all_adjacency(self):
+        """
+        Generate adjacency board for each cell in gameboard
+        """
         # Iterate through each cell
         for i in range(len(self.gameboard)):
             for j in range(len(self.gameboard[i])):
                 # If cell is itself a mine, do nothing
-                if self.gameboard[i][j] != "X":
+                if self.gameboard[i][j] != "MINE!":
 
                     adjacenct_count = 0
                     valid_neighbors = self.get_valid_neighbors(i, j)
@@ -62,13 +85,16 @@ class Minesweeper:
                     for x, y in valid_neighbors:
                         # print(f"x: {x}, y: {y}")
                         # print(f"Gameboard at {x}, {y}: {self.gameboard[x][y]}")
-                        if self.gameboard[x][y] == "X":
+                        if self.gameboard[x][y] == "MINE!":
                             adjacenct_count += 1
                             # print(f"Adjacency count: {adjacenct_count}")
                     self.gameboard[i][j] = adjacenct_count
 
-    # Helper function: is valid position
     def _is_valid(self, row_position, col_position):
+        """
+        Determine if a position is valid
+        Internal helper function
+        """
         # Check if x is in position
         if 0 <= row_position <= self.width - 1 and 0 <= col_position <= self.height - 1:
             return True
@@ -94,10 +120,11 @@ class Minesweeper:
 
 
 new_game = Minesweeper(5, 5)
-new_game.add_mine(0, 0)
-new_game.add_mine(0, 1)
-new_game.add_mine(1, 2)
-new_game.add_mine(3, 4)
+new_game.add_random_mines(5)
+# new_game.add_mine(0, 0)
+# new_game.add_mine(0, 1)
+# new_game.add_mine(1, 2)
+# new_game.add_mine(3, 4)
 print("Fresh gameboard:")
 pprint(new_game.gameboard)
 # print(f"Valid neighbors for (1, 1): {new_game.get_valid_neighbors(1, 1)}")
